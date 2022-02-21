1e4b19cc7e9013f6f99da99bcfd01d30c7ecb239
Joey Turczak <joeyturczak@gmail.com>
2022-02-20 21:17:44 -0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,9 +24,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        if($this->app->environment('production'))
-        {
-            \URL::forceScheme('https');
-        }
     }
 }
