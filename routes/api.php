5e19dd8fe9fdfb6d34f8df9b3bf0743da91f6853
Joey Turczak <joeyturczak@gmail.com>
2022-02-26 22:20:11 -0800

    Add event data

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/events-feed', function () {
+    return response()->file(resoure_path('assets/js/data/events.json'));
+});
