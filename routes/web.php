96f0266a1c5cd5086b9882d1e180f607aaf940eb
Joey Turczak <joeyturczak@gmail.com>
2022-02-26 23:05:38 -0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/home', function () {
+    return view('welcome');
+});
+
 Route::get('/todos', function () {
     return view('todos');
 });
