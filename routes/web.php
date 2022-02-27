422366a64cd31af21e83a2ae080cefd08f5589d9
Joey Turczak <joeyturczak@gmail.com>
2022-02-26 18:15:50 -0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/todos', function () {
+    return view('todos');
+});
+
+Route::get('/calendar', function () {
+    return view('calendar');
+});
+
+Route::get('/board', function () {
+    return view('board');
+});
