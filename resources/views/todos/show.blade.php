07c853160e6321bcfd46b2c030e49e8476905f85
Joey Turczak <joeyturczak@gmail.com>
2022-03-11 20:34:28 -0800

    Add Todo views

---
+++ resources/views/todos/show.blade.php
@@ -0,0 +1,14 @@
+@extends('adminlte::page')
+
+@section('title', 'To Do List')
+
+@section('content_header')
+  <h1>To Dos</h1>
+@stop
+
+@section('content')
+  <h2>{{ $todo->title; }}</h2>
+  <div>
+    <p>{{ $todo.progress; }}% finished</p>
+  </div>
+@stop
